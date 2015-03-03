a3eef669ab74e9ad29a1430ca929cd9df4b01578
Lei Chen <raistlic@gmail.com>
2015-03-03 21:20:01 +1100

    updated the license information.

--- src/main/java/org/raistlic/common/event/Listener.java
+++ src/main/java/org/raistlic/common/event/Listener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Lei CHEN (raistlic@gmail.com)
+ * Copyright 2015 Lei CHEN (raistlic@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
