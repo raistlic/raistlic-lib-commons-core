e1c55f99514ab2955cbb643e7c6b21c408976e7f
Lei Chen <raistlic@gmail.com>
2015-10-13 17:38:04 +1100

    PredicateOrWrapper javadoc and unit test done.

--- src/main/java/org/raistlic/common/util/PredicateOrWrapper.java
+++ src/main/java/org/raistlic/common/util/PredicateOrWrapper.java
@@ -21,16 +21,28 @@ import org.raistlic.common.precondition.Precondition;
 import java.util.function.Predicate;
 
 /**
+ * A wrapper class that wraps two {@link Predicate} instances, and returns {@code true} as long as
+ * any one of the tests passes.
+ *
  * @author Lei Chen (2015-10-13)
  * @since 1.3
  */
-public final class PridicateOrWrapper<E> implements Predicate<E> {
+public final class PredicateOrWrapper<E> implements Predicate<E> {
 
   private final Predicate<? super E> left;
 
   private final Predicate<? super E> right;
 
-  public PridicateOrWrapper(Predicate<? super E> left, Predicate<? super E> right) {
+  /**
+   * Wraps the two {@link Predicate} instances to create the wrapper object.
+   *
+   * @param left the left operand of the wrapper, cannot be {@code null}.
+   * @param right the right operand of the wrapper, cannot be {@code null}.
+   *
+   * @throws org.raistlic.common.precondition.InvalidParameterException when any of the parameters
+   *         is {@code null}.
+   */
+  public PredicateOrWrapper(Predicate<? super E> left, Predicate<? super E> right) {
 
     Precondition.param(left, "left").notNull();
     Precondition.param(right, "right").notNull();
---
+++ src/test/java/org/raistlic/common/util/PredicateOrWrapperTest.java
@@ -0,0 +1,99 @@
+package org.raistlic.common.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.raistlic.common.precondition.InvalidParameterException;
+
+import java.util.function.Predicate;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Lei Chen (2015-10-13)
+ */
+@RunWith(JUnit4.class)
+@SuppressWarnings("unchecked")
+public class PredicateOrWrapperTest {
+
+  @Test(expected = InvalidParameterException.class)
+  public void constructorWithNullLeft() {
+
+    new PredicateOrWrapper<Object>(null, Predicates.dummyTrue());
+  }
+
+  @Test(expected = InvalidParameterException.class)
+  public void constructorWithNullRight() {
+
+    new PredicateOrWrapper<Object>(Predicates.dummyTrue(), null);
+  }
+
+  @Test
+  public void testWhenBothTestsFalse() {
+
+    Object target = new Object();
+    Predicate<Object> left = mock(Predicate.class);
+    Predicate<Object> right = mock(Predicate.class);
+    doReturn(false).when(left).test(any());
+    doReturn(false).when(right).test(any());
+
+    boolean actual = new PredicateOrWrapper<Object>(left, right).test(target);
+
+    assertThat(actual).isFalse();
+    verify(left).test(target);
+    verify(right).test(target);
+  }
+
+  @Test
+  public void testWhenLeftTestTrue() {
+
+    Object target = new Object();
+    Predicate<Object> left = mock(Predicate.class);
+    Predicate<Object> right = mock(Predicate.class);
+    doReturn(true).when(left).test(any());
+    doReturn(false).when(right).test(any());
+
+    boolean actual = new PredicateOrWrapper<Object>(left, right).test(target);
+
+    assertThat(actual).isTrue();
+    verify(left).test(target);
+    verify(right, never()).test(target);
+  }
+
+  @Test
+  public void testWhenRightTestTrue() {
+
+    Object target = new Object();
+    Predicate<Object> left = mock(Predicate.class);
+    Predicate<Object> right = mock(Predicate.class);
+    doReturn(false).when(left).test(any());
+    doReturn(true).when(right).test(any());
+
+    boolean actual = new PredicateOrWrapper<Object>(left, right).test(target);
+
+    assertThat(actual).isTrue();
+    verify(left).test(target);
+    verify(right).test(target);
+  }
+
+  @Test
+  public void testWhenBothTestsTrue() {
+
+    Object target = new Object();
+    Predicate<Object> left = mock(Predicate.class);
+    Predicate<Object> right = mock(Predicate.class);
+    doReturn(true).when(left).test(any());
+    doReturn(true).when(right).test(any());
+
+    boolean actual = new PredicateOrWrapper<Object>(left, right).test(target);
+
+    assertThat(actual).isTrue();
+    verify(left).test(target);
+    verify(right, never()).test(target);
+  }
+}
