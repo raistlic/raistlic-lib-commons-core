447ba68973531747f2b9154982888deebe2a3f0d
Lei Chen <raistlic@gmail.com>
2016-03-17 11:40:16 +1100

    CollectionPredicates added.

---
+++ src/main/java/org/raistlic/common/predicate/CollectionPredicates.java
@@ -0,0 +1,84 @@
+package org.raistlic.common.predicate;
+
+import org.raistlic.common.precondition.Precondition;
+
+import java.util.Collection;
+import java.util.function.Predicate;
+
+/**
+ * Static factory methods holder that exposes different kinds of {@link Predicate} instances for
+ * testing {@link Collection} .
+ *
+ * @author Lei Chen (2016-03-17)
+ */
+public final class CollectionPredicates {
+
+  public static Predicate<Collection<?>> isEmpty() {
+
+    return CollectionIsEmptyPredicate.INSTANCE;
+  }
+
+  public static Predicate<Collection<?>> notEmpty() {
+
+    return Predicates.not(isEmpty());
+  }
+
+  public static Predicate<Collection<?>> hasSize(int size) {
+
+    Precondition.param(size, "size").greaterThanOrEqualTo(0);
+    return new CollectionWithSizePredicate(size);
+  }
+
+  public static <E> Predicate<Collection<E>> contains(E element) {
+
+    return new CollectionContainsPredicate<>(element);
+  }
+
+  private enum CollectionIsEmptyPredicate implements Predicate<Collection<?>> {
+
+    INSTANCE;
+
+    @Override
+    public boolean test(Collection<?> collection) {
+
+      return collection != null && collection.isEmpty();
+    }
+  }
+
+  private static class CollectionWithSizePredicate implements Predicate<Collection<?>> {
+
+    private final int size;
+
+    private CollectionWithSizePredicate(int size) {
+
+      this.size = size;
+    }
+
+    @Override
+    public boolean test(Collection<?> objects) {
+
+      return objects != null && objects.size() == size;
+    }
+  }
+
+  private static class CollectionContainsPredicate<E> implements Predicate<Collection<E>> {
+
+    private final E element;
+
+    private CollectionContainsPredicate(E element) {
+
+      this.element = element;
+    }
+
+    @Override
+    public boolean test(Collection<E> es) {
+
+      return es != null && es.contains(element);
+    }
+  }
+
+  /*
+   * Not to be instantiated or inherited.
+   */
+  private CollectionPredicates() { }
+}
