84cb10d2e1a3a81b7232ee6e2647911d1c9d6902
Lei Chen <raistlic@gmail.com>
2021-04-08 09:41:06 +1000

    try fix maven publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -ex
-
 mkdir -pv tmp
 gcloud secrets versions access latest --secret=settings-maven-central-publish | base64 --decode > tmp/settings.xml
 gcloud secrets versions access latest --secret=credentials-maven-central-private-key > tmp/key.pem
@@ -10,11 +8,5 @@ MAVEN_USERNAME=$(gcloud secrets versions access latest --secret=credentials-mave
 MAVEN_PASSWORD=$(gcloud secrets versions access latest --secret=credentials-maven-central-password)
 SIGNING_KID=$(gcloud secrets versions access latest --secret=credentials-maven-central-signing-keyid)
 SIGNING_PASSWORD=$(gcloud secrets versions access latest --secret=credentials-maven-central-signing-password)
-
-echo "maven username: $MAVEN_USERNAME"
-echo "maven password: $MAVEN_PASSWORD"
-echo "signing kid: $SIGNING_KID"
-echo "signing password: $SIGNING_PASSWORD"
-echo "signing key: $(cat tmp/key.pem)"
 
 ./gradlew clean build uploadArchives -Pmaven.username=${MAVEN_USERNAME} -Pmaven.password=${MAVEN_PASSWORD} -Psigning.secretKeyRingFile=./tmp/key.pem -Psigning.password=${SIGNING_PASSWORD} -Psigning.keyId=${SIGNING_KID}
