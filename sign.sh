9cd2a5ce71d966601029aaf2b78596845dbddb40
Lei Chen <raistlic@gmail.com>
2021-04-09 09:14:36 +1000

    [#21] fix maven publish signing 3rd try

--- sign.sh
+++ sign.sh
@@ -2,11 +2,11 @@
 
 mkdir -pv tmp
 gcloud secrets versions access latest --secret=settings-maven-central-publish | base64 --decode > tmp/settings.xml
-gcloud secrets versions access 3 --secret=credentials-maven-central-private-key | base64 --decode | tr , '\n' > tmp/key.pem
+gcloud secrets versions access latest --secret=credentials-maven-central-private-key | base64 --decode | tr , '\n' > tmp/key.pem
 
 MAVEN_USERNAME=$(gcloud secrets versions access latest --secret=credentials-maven-central-username)
 MAVEN_PASSWORD=$(gcloud secrets versions access latest --secret=credentials-maven-central-password)
 SIGNING_KID=$(gcloud secrets versions access latest --secret=credentials-maven-central-signing-keyid)
 SIGNING_PASSWORD=$(gcloud secrets versions access latest --secret=credentials-maven-central-signing-password)
 
-./gradlew clean build publish -Pmaven.username=${MAVEN_USERNAME} -Pmaven.password=${MAVEN_PASSWORD} -Psigning.secretKeyFile=./tmp/key.pem -Psigning.password=${SIGNING_PASSWORD} -Psigning.keyId=${SIGNING_KID}
+./gradlew clean build sign -x test -Pmaven.username=${MAVEN_USERNAME} -Pmaven.password=${MAVEN_PASSWORD} -Psigning.secretKeyFile=./tmp/key.pem -Psigning.password=${SIGNING_PASSWORD} -Psigning.keyId=${SIGNING_KID}
